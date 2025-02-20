0bf92cfba40bdd0fbf9b265addd875f7301aa02d
Arfan Roky <arfanroky.dev@gmail.com>
2025-02-20 14:08:57 +0600

    feat: update DashboardView to include header, summary, and stats components

--- src/sections/dashboard/views/dashboard-view.tsx
+++ src/sections/dashboard/views/dashboard-view.tsx
@@ -1,5 +1,15 @@
-'use client';
+import DashboardHeaderView from "./dashboard-header-view";
+import SummaryContainerView from "./summary-container-view";
+import StatsContainerView from "./stats-container-view";
+import { Stack } from "@mui/material";
 
-export default function DashboardView(){
-  return <>Dashboard View</>;
+export default function DashboardPage() {
+  return (
+    <Stack direction={"column"} gap={"24px"}>
+      <DashboardHeaderView />
+
+      <SummaryContainerView />
+      <StatsContainerView />
+    </Stack>
+  );
 }
